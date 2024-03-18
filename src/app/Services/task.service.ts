c60050e9bbf50a926b3acf9e27d6c6cc8f456ca1
a-rRehman <70515831+a-rRehman@users.noreply.github.com>
2024-03-18 16:36:06 +0500

    Service Added For all methods

---
+++ src/app/Services/task.service.ts
@@ -0,0 +1,65 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable, inject } from "@angular/core";
+import { Task } from "../Model/task";
+import { HttpHeaders } from "@angular/common/http";
+import { map } from "rxjs/operators";
+
+@Injectable({
+  providedIn: "root",
+})
+export class TaskService {
+  http: HttpClient = inject(HttpClient);
+  CreateTask(task: Task) {
+    const headers = new HttpHeaders({ my_headers: "HelloWorld" });
+    this.http
+      .post<{ name: string }>(
+        "https://angualrhttpclient-default-rtdb.firebaseio.com/tasks.json",
+        task,
+        {
+          headers: headers,
+        }
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  deleteRecord(id: string | undefined) {
+    this.http
+      .delete(
+        "https://angualrhttpclient-default-rtdb.firebaseio.com/tasks/" +
+          id +
+          ".json"
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+  deleteRecords() {
+    this.http
+      .delete(
+        "https://angualrhttpclient-default-rtdb.firebaseio.com/tasks.json"
+      )
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+  GetAlltasks() {
+    return this.http
+      .get<{ [key: string]: Task }>(
+        "https://angualrhttpclient-default-rtdb.firebaseio.com/tasks.json"
+      )
+      .pipe(
+        map((response) => {
+          //Transform Data
+          let tasks = [];
+          for (let key in response) {
+            if (response.hasOwnProperty(key)) {
+              tasks.push({ ...response[key], id: key });
+            }
+          }
+          return tasks;
+        })
+      );
+  }
+}
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, inject } from "@angular/core";
 import { Task } from "../Model/task";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { map } from "rxjs/operators";
+import { TaskService } from "../Services/task.service";
 
 @Component({
   selector: "app-dashboard",
@@ -12,6 +13,7 @@ export class DashboardComponent implements OnInit {
   showCreateTaskForm: boolean = false;
   http: HttpClient = inject(HttpClient);
   allTasks: Task[] = [];
+  taskService: TaskService = inject(TaskService);
   OpenCreateTaskForm() {
     this.showCreateTaskForm = true;
   }
@@ -20,66 +22,22 @@ export class DashboardComponent implements OnInit {
     this.showCreateTaskForm = false;
   }
   createTask(data: Task) {
-    const headers = new HttpHeaders({ my_headers: "HelloWorld" });
-    console.log("Dashboard", data);
-    this.http
-      .post<{ name: string }>(
-        "https://angualrhttpclient-default-rtdb.firebaseio.com/tasks.json",
-        data,
-        {
-          headers: headers,
-        }
-      )
-      .subscribe((response) => {
-        console.log(response);
-        this.fetchAlltasks();
-      });
+    this.taskService.CreateTask(data);
   }
   deleteRecord(id: string | undefined) {
-    this.http
-      .delete(
-        "https://angualrhttpclient-default-rtdb.firebaseio.com/tasks/" +
-          id +
-          ".json"
-      )
-      .subscribe((response) => {
-        console.log(response);
-        this.fetchAlltasks();
-      });
+    this.taskService.deleteRecord(id);
   }
   deleteAllRecords() {
-    this.http
-      .delete(
-        "https://angualrhttpclient-default-rtdb.firebaseio.com/tasks.json"
-      )
-      .subscribe((response) => {
-        console.log(response);
-        this.fetchAlltasks();
-      });
+    this.taskService.deleteRecords();
   }
+
   fetchTasks() {
     this.fetchAlltasks();
   }
   fetchAlltasks() {
-    this.http
-      .get<{ [key: string]: Task }>(
-        "https://angualrhttpclient-default-rtdb.firebaseio.com/tasks.json"
-      )
-      .pipe(
-        map((response) => {
-          //Transform Data
-          let tasks = [];
-          for (let key in response) {
-            if (response.hasOwnProperty(key)) {
-              tasks.push({ ...response[key], id: key });
-            }
-          }
-          return tasks;
-        })
-      )
-      .subscribe((tasks) => {
-        this.allTasks = tasks;
-      });
+    this.taskService.GetAlltasks().subscribe((tasks) => {
+      this.allTasks = tasks;
+    });
   }
   ngOnInit(): void {
     this.fetchAlltasks();
